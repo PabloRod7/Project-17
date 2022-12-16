7048d2036ab333b3c674cb7c24bd20637f606915
Pablo2maker <pablo-nox1@hotmail.com>
2022-12-16 11:03:45 +0100

    new Commit

--- Project 17/GameScene.swift
+++ Project 17/GameScene.swift
@@ -6,83 +6,211 @@
 //
 
 import SpriteKit
-import GameplayKit
 
-class GameScene: SKScene {
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var startField : SKEmitterNode!
+    var player : SKSpriteNode!
+    var scoreLabel : SKLabelNode!
+    var gameoverLabel : SKLabelNode!
+    var newgameLabel : SKLabelNode!
+    
+    
+    var gameTimer : Timer?
+    let possibleEnemy = ["tv", "hammer", "ball"]
+    var isGameOver = false
+    
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score \(score)"
+        }
+    }
+    
+    var timerLoop = 0
+    var timerInterval : Double = 1
+    
     
     override func didMove(to view: SKView) {
+        backgroundColor = .black
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        startField = SKEmitterNode(fileNamed: "starfield")
+        startField.position = CGPoint(x: 586, y: 150)
+        startField.advanceSimulationTime(10)
+        addChild(startField)
+        startField.zPosition = -1
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: -293, y: 0)
+        player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
+        player.physicsBody?.contactTestBitMask = 1
+        addChild(player)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.position = CGPoint(x: -300, y: -250)
+        scoreLabel.horizontalAlignmentMode = .left
+        addChild(scoreLabel)
+        score = 0
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        
+        
+        
+        newGame()
+        physicsWorld.gravity = .zero
+        physicsWorld.contactDelegate = self
+        
+        
+        
+        
+        
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    func didBegin(_ contact: SKPhysicsContact) {
+        guard !isGameOver else {return}
+        gameOver()
+        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        guard let touch = touches.first else {return}
+        
+        if !isGameOver {
+            gameOver()
+            return
+        }
+        
+        let location = touch.location(in: self)
+        let objects = nodes(at: location)
+        for object in objects {
+            if object.name == "newgame" {
+                newGame()
+            }
+        }
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
     
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        for node in children {
+            if node.position.x < -1024 {
+                node.removeFromParent()
+            }
+            
+            
+        }
+        
+        if !isGameOver {
+            score += 1
+        }
     }
+    
+    @objc func createEnemy() {
+        timerLoop += 1
+        
+        guard let enemy = possibleEnemy.randomElement() else {return}
+        
+        let sprite = SKSpriteNode(imageNamed: enemy)
+        sprite.position = CGPoint(x: 1024, y: Int.random(in: -386...384))
+        sprite.name = "Enemy"
+        addChild(sprite)
+        
+        sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
+        sprite.physicsBody?.categoryBitMask = 1
+        sprite.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
+        sprite.physicsBody?.angularVelocity = 5
+        sprite.physicsBody?.linearDamping = 0
+        sprite.physicsBody?.angularDamping = 0
+        
+        if timerLoop >= 20 {
+            timerLoop = 0
+            
+            if timerInterval >= 0.2 {
+                timerInterval = 0.1
+            }
+            
+            gameTimer?.invalidate()
+            
+            gameTimer = Timer.scheduledTimer(timeInterval: timerInterval, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
+        }
+    }
+
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        guard let touch = touches.first else {return}
+        var location = touch.location(in: self)
+        
+        if location.y < -250 {
+            location.y = -250
+        } else if location.y > 250 {
+            location.y = 250
+        }
+        
+        player.position = location
+        
+    }
+    func gameOver() {
+        let explosion = SKEmitterNode(fileNamed: "explosion")!
+        explosion.position = player.position
+        addChild(explosion)
+        
+        player.removeFromParent()
+        isGameOver = true
+        
+        
+        gameTimer?.invalidate()
+        
+        gameoverLabel = SKLabelNode(fontNamed: "Chalkduster")
+        gameoverLabel.position = CGPoint(x: 0, y: 0)
+        gameoverLabel.horizontalAlignmentMode = .center
+        gameoverLabel?.fontSize = 50
+        gameoverLabel.zPosition = 1
+        gameoverLabel.color = .red
+        gameoverLabel.text = "GAME OVER"
+        addChild(gameoverLabel)
+        
+        newgameLabel = SKLabelNode(fontNamed: "Chalkduster")
+        newgameLabel.position = CGPoint(x: -0, y: -150)
+        newgameLabel.zPosition = 1
+        newgameLabel.horizontalAlignmentMode = .center
+        newgameLabel.fontSize = 40
+        newgameLabel.name = "newgame"
+        newgameLabel.text = "NEW GAME"
+        addChild(newgameLabel)
+        
+    }
+    
+    func newGame() {
+        guard isGameOver else { return }
+        
+        score = 0
+        timerInterval = 1
+        timerLoop = 0
+        
+        isGameOver = false
+        
+        if let gameoverLabel = gameoverLabel {
+            gameoverLabel.removeFromParent()
+        }
+        if let newgameLabel = newgameLabel {
+            newgameLabel.removeFromParent()
+        }
+        
+        for node in children {
+           if node.name == "Enemy" {
+                node.removeFromParent()
+            }
+            
+        }
+        
+        player.position = CGPoint(x: -293, y: 0)
+        addChild(player)
+        
+        gameTimer = Timer.scheduledTimer(timeInterval: timerInterval, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
+        
+    }
+    
+    
+    
+    
 }
